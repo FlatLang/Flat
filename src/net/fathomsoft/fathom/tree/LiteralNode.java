abe49be4b1560e7e675416a6a2d9f59da856aa14
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-03-13 05:54:24 -0500

    New isString() method to check if the given variable is a String literal. New isNumber() method to check if the given variable is a Number literal.

--- src/net/fathomsoft/fathom/tree/LiteralNode.java
+++ src/net/fathomsoft/fathom/tree/LiteralNode.java
@@ -54,4 +54,73 @@ public class LiteralNode extends TreeNode
 	{
 		return value;
 	}
+	
+	public static boolean isString(String value)
+	{
+		if (value.length() < 2)
+		{
+			return false;
+		}
+		
+		return value.charAt(0) == '"' && value.charAt(value.length() - 1) == '"';
+	}
+	
+	public static boolean isNumber(String value)
+	{        
+	    boolean seenDot     = false;
+	    boolean seenExp     = false;
+	    boolean justSeenExp = false;
+	    boolean seenDigit   = false;
+	    
+	    for (int i = 0; i < value.length(); i++)
+	    {
+	        char c = value.charAt(i);
+	        
+	        if (c >= '0' && c <= '9')
+	        {
+	            seenDigit = true;
+	            continue;
+	        }
+	        
+	        if ((c == '-' || c == '+') && (i == 0 || justSeenExp))
+	        {
+	            continue;
+	        }
+	        
+	        if (c == '.' && !seenDot)
+	        {
+	            seenDot = true;
+	            
+	            continue;
+	        }
+	        
+	        justSeenExp = false;
+	        
+	        if ((c == 'e' || c == 'E') && !seenExp)
+	        {
+	            seenExp     = true;
+	            justSeenExp = true;
+	            
+	            continue;
+	        }
+	        
+	        return false;
+	    }
+	    
+	    if (!seenDigit)
+	    {
+	        return false;
+	    }
+	    
+	    try
+	    {
+	        Double.parseDouble(value);
+	        
+	        return true;
+	    }
+	    catch (NumberFormatException e)
+	    {
+	        return false;
+	    }
+	}
 }
