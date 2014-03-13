e50998c460fcec5106e6a6591f8d5f037016ff17
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-03-13 05:52:40 -0500

    Added a rough draft for the decodeStatement method.

--- src/net/fathomsoft/fathom/tree/IfStatementNode.java
+++ src/net/fathomsoft/fathom/tree/IfStatementNode.java
@@ -1,5 +1,10 @@
 package net.fathomsoft.fathom.tree;
 
+import net.fathomsoft.fathom.util.Bounds;
+import net.fathomsoft.fathom.util.Location;
+import net.fathomsoft.fathom.util.Patterns;
+import net.fathomsoft.fathom.util.Regex;
+
 /**
  * 
  * 
@@ -40,6 +45,33 @@ public class IfStatementNode extends TreeNode
 	@Override
 	public String generateCSourceOutput()
 	{
+		return null;
+	}
+	
+	public static IfStatementNode decodeStatement(TreeNode parent, String statement, Location location)
+	{
+		if (Regex.matches(statement, 0, Patterns.PRE_IF))
+		{
+			IfStatementNode n = new IfStatementNode();
+			
+			Bounds bounds = Regex.boundsOf(statement, Patterns.POST_IF);
+			
+			String contents = statement.substring(bounds.getStart(), bounds.getEnd());
+			
+			if (returnStartIndex >= 0)
+			{
+				statement = statement.substring(returnStartIndex);
+				
+				Location newLoc = new Location();
+				newLoc.setLineNumber(location.getLineNumber());
+				newLoc.setOffset(location.getOffset() + returnStartIndex);
+				
+				TreeNode child = BinaryOperatorNode.decodeStatement(parent, statement, newLoc);
+				
+				n.addChild(child);
+			}
+		}
+		
 		return null;
 	}
 }
