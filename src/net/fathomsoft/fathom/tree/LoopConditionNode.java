beeb303f232d1e03fe2bcb00864586a04a7d1735
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-03-16 16:12:12 -0500

    New clone() method.

--- src/net/fathomsoft/fathom/tree/LoopConditionNode.java
+++ src/net/fathomsoft/fathom/tree/LoopConditionNode.java
@@ -42,4 +42,22 @@ public class LoopConditionNode extends TreeNode
 	{
 		return null;
 	}
+
+	/**
+	 * @see net.fathomsoft.fathom.tree.TreeNode#clone()
+	 */
+	@Override
+	public LoopConditionNode clone()
+	{
+		LoopConditionNode clone = new LoopConditionNode();
+		
+		for (int i = 0; i < getChildren().size(); i++)
+		{
+			TreeNode child = getChild(i);
+			
+			clone.addChild(child.clone());
+		}
+		
+		return clone;
+	}
 }
