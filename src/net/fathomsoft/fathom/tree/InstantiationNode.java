1dbe4107e405688293a4f01d712e11547103354f
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-03-16 16:08:29 -0500

    Created c source output. Created decodeStatement() method that decodes an instantiation. Created clone() method.

--- src/net/fathomsoft/fathom/tree/InstantiationNode.java
+++ src/net/fathomsoft/fathom/tree/InstantiationNode.java
@@ -1,8 +1,10 @@
 package net.fathomsoft.fathom.tree;
 
+import net.fathomsoft.fathom.error.SyntaxMessage;
 import net.fathomsoft.fathom.util.Location;
 import net.fathomsoft.fathom.util.Patterns;
 import net.fathomsoft.fathom.util.Regex;
+import net.fathomsoft.fathom.util.SyntaxUtils;
 
 public class InstantiationNode extends TreeNode
 {
@@ -30,33 +32,70 @@ public class InstantiationNode extends TreeNode
 	@Override
 	public String generateCSourceOutput()
 	{
-		return null;
+		StringBuilder builder = new StringBuilder();
+		
+		for (int i = 0; i < getChildren().size(); i++)
+		{
+			builder.append(getChild(i).generateCSourceOutput());
+		}
+		
+		return builder.toString();
 	}
 	
-	public static ReturnNode decodeStatement(TreeNode parent, String statement, Location location)
+	public static InstantiationNode decodeStatement(TreeNode parent, String statement, Location location)
 	{
-		ReturnNode n = null;
+		InstantiationNode n = null;
 		
-		if (Regex.indexOf(statement, Patterns.PRE_INSTANTIATION) == 0)
+		if (SyntaxUtils.isInstantiation(statement))
 		{
-			n = new ReturnNode();
+			n = new InstantiationNode();
 			
-			int returnStartIndex = Regex.indexOf(statement, Patterns.POST_INSTANTIATION);
+			int startIndex = Regex.indexOf(statement, Patterns.POST_INSTANTIATION);
 			
-			if (returnStartIndex >= 0)
+			statement = statement.substring(startIndex);
+			
+			Location newLoc = new Location();
+			newLoc.setLineNumber(location.getLineNumber());
+			newLoc.setOffset(location.getOffset() + startIndex);
+			
+			TreeNode child = null;
+			
+			if (SyntaxUtils.isMethodCall(statement))
 			{
-				statement = statement.substring(returnStartIndex);
-				
-				Location newLoc = new Location();
-				newLoc.setLineNumber(location.getLineNumber());
-				newLoc.setOffset(location.getOffset() + returnStartIndex);
-				
-				TreeNode child = MethodCallNode.decodeStatement(parent, statement, newLoc);
-				
-				n.addChild(child);
+				child = MethodCallNode.decodeStatement(parent, statement, newLoc);
 			}
+			else if (SyntaxUtils.isArrayInitialization(statement))
+			{
+				child = ArrayNode.decodeStatement(parent, statement, newLoc);
+			}
+			else
+			{
+				SyntaxMessage.error("Unable to parse instantiation", newLoc);
+				
+				return null;
+			}
+			
+			n.addChild(child);
 		}
 		
 		return n;
+	}
+
+	/**
+	 * @see net.fathomsoft.fathom.tree.TreeNode#clone()
+	 */
+	@Override
+	public InstantiationNode clone()
+	{
+		InstantiationNode clone = new InstantiationNode();
+		
+		for (int i = 0; i < getChildren().size(); i++)
+		{
+			TreeNode child = getChild(i);
+			
+			clone.addChild(child.clone());
+		}
+		
+		return clone;
 	}
 }
