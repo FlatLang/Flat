e4b3111caa78965eb22a53ee05a90c5be8e9b436
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-03-16 16:03:08 -0500

    New clone() method.

--- src/net/fathomsoft/fathom/tree/FieldListNode.java
+++ src/net/fathomsoft/fathom/tree/FieldListNode.java
@@ -135,4 +135,22 @@ public class FieldListNode extends TreeNode
 		
 		return builder.toString();
 	}
+
+	/**
+	 * @see net.fathomsoft.fathom.tree.TreeNode#clone()
+	 */
+	@Override
+	public FieldListNode clone()
+	{
+		FieldListNode clone = new FieldListNode();
+		
+		for (int i = 0; i < getChildren().size(); i++)
+		{
+			TreeNode child = getChild(i);
+			
+			clone.addChild(child.clone());
+		}
+		
+		return clone;
+	}
 }
