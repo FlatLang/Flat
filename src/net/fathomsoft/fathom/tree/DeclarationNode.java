38a88364f41ef0552b0c4aa1be947d4d08e8a189
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-03-16 16:02:32 -0500

    New clone() method.

--- src/net/fathomsoft/fathom/tree/DeclarationNode.java
+++ src/net/fathomsoft/fathom/tree/DeclarationNode.java
@@ -183,13 +183,29 @@ public class DeclarationNode extends VariableNode
 		
 		return builder.toString();
 	}
-
+	
 	/**
-	 * @see net.fathomsoft.fathom.tree.TreeNode#generateCSourceOutput()
+	 * @see net.fathomsoft.fathom.tree.TreeNode#clone()
 	 */
 	@Override
-	public String generateCSourceOutput()
+	public DeclarationNode clone()
 	{
-		return null;
+		DeclarationNode clone = new DeclarationNode();
+		clone.setStatic(isStatic());
+		clone.setVisibility(getVisibility());
+		clone.setConst(isConst());
+		clone.setArrayDimensions(getArrayDimensions());
+		clone.setType(getType());
+		clone.setReference(isReference());
+		clone.setPointer(isPointer());
+		
+		for (int i = 0; i < getChildren().size(); i++)
+		{
+			TreeNode child = getChild(i);
+			
+			clone.addChild(child.clone());
+		}
+		
+		return clone;
 	}
 }
