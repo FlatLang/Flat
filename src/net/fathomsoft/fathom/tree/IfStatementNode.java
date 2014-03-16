9ff177fbd94f95a3c7f1723acf23a3fb5e3e9720
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-03-16 16:05:58 -0500

    New clone() method.

--- src/net/fathomsoft/fathom/tree/IfStatementNode.java
+++ src/net/fathomsoft/fathom/tree/IfStatementNode.java
@@ -79,4 +79,22 @@ public class IfStatementNode extends TreeNode
 		
 		return null;
 	}
+
+	/**
+	 * @see net.fathomsoft.fathom.tree.TreeNode#clone()
+	 */
+	@Override
+	public IfStatementNode clone()
+	{
+		IfStatementNode clone = new IfStatementNode();
+		
+		for (int i = 0; i < getChildren().size(); i++)
+		{
+			TreeNode child = getChild(i);
+			
+			clone.addChild(child.clone());
+		}
+		
+		return clone;
+	}
 }
