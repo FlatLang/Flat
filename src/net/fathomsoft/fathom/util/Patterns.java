fc02ddde2dab20dc1f284c6813fa1f0b41b348ed
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-03-19 06:32:12 -0500

    Updated PRE_EQUALS_SIGN pattern to work faster. Updated BINARY_ARITH_OPERATORS and PRE_OPERATORS to include the == operator. Updated POST_IF to work correctly. Added MULTI_LINE_COMMENT and COMMENT to do their respective things.

--- src/net/fathomsoft/fathom/util/Patterns.java
+++ src/net/fathomsoft/fathom/util/Patterns.java
@@ -28,17 +28,17 @@ public class Patterns
 	public static final Pattern	STATEMENT_END			= Pattern.compile("\\s*[\\{\\;]");
 	public static final Pattern	NEXT_TEXT_LINE			= Pattern.compile("(.)", Pattern.MULTILINE);
 	public static final Pattern FORMATTER_PATTERN		= Pattern.compile("[\\}\\{\\)\\(\\n]");
-	public static final Pattern PRE_EQUALS_SIGN			= Pattern.compile("(\\S+\\s*)+=");
+	public static final Pattern PRE_EQUALS_SIGN			= Pattern.compile("([A-Za-z0-9_\\[\\]]+\\s*)+=");
 	public static final Pattern POST_EQUALS_SIGN		= Pattern.compile("(?<=(=\\s{0,9}))(?=\\S+)");
-	public static final Pattern BINARY_ARITH_OPERATORS	= Pattern.compile("[\\*\\+\\/\\-=]");
+	public static final Pattern BINARY_ARITH_OPERATORS	= Pattern.compile("(==|[\\*\\+\\/\\-=])");
 	public static final Pattern BINARY_LOGIC_OPERATORS	= Pattern.compile("[\\|\\&]");
-	public static final Pattern PRE_OPERATORS			= Pattern.compile("\\s+[\\*\\+\\/\\-]");
+	public static final Pattern PRE_OPERATORS			= Pattern.compile("\\s+(==|[\\*\\+\\/\\-=])");
 	public static final Pattern WHITESPACE				= Pattern.compile("\\s");
 	public static final Pattern NON_WHITESPACE			= Pattern.compile("\\S");
 	public static final Pattern PRE_RETURN				= Pattern.compile("return\\s*");
 	public static final Pattern POST_RETURN     	    = Pattern.compile("(?<=return\\s{0,9})(\\S+)");
 	public static final Pattern PRE_IF					= Pattern.compile("if\\s*\\(");
-	public static final Pattern POST_IF	     	    	= Pattern.compile("(?<=if\\s{0,9}\\(\\s{0,9})(\\S+\\))");
+	public static final Pattern POST_IF	     	    	= Pattern.compile("(?<=if\\s{0,9}\\(\\s{0,9})(.+)(?=\\s*\\))");
 	public static final Pattern PRE_IMPORT          	= Pattern.compile("import\\s*");
 	public static final Pattern POST_IMPORT				= Pattern.compile("(?<=import\\s{0,9})(\\S+)");
 	public static final Pattern PRE_INSTANTIATION		= Pattern.compile("new\\s+\\S+");
@@ -53,4 +53,6 @@ public class Patterns
 	public static final Pattern ARRAY_BRACKETS     	    = Pattern.compile("\\s*\\[\\s*\\]");
 	public static final Pattern METHOD_NAME     	    = Pattern.compile("\\S+(?=\\s*\\()");
 	public static final Pattern SINGLE_LINE_COMMENT	    = Pattern.compile("//.*");
+	public static final Pattern MULTI_LINE_COMMENT	    = Pattern.compile("/\\*(.|[\r\n])*?\\*/");
+	public static final Pattern COMMENT					= Pattern.compile("(//.*)|(/\\*(.|[\r\n])*?\\*/)");
 }
