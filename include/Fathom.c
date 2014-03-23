e3653ce84115cf9368efe66e0fa66a71a87e3409
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-03-23 17:21:52 -0500

    New files used for part of the Fathom c util library.

---
+++ include/Fathom.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+/*
+ * Similar to fgets(), but handles automatic reallocation of the buffer.
+ * Only parameter is the input stream.
+ * Return value is a string. Don't forget to free it.
+ */
+char* ufgets(FILE* stream)
+{
+    unsigned int maxlen = 128, size = 128;
+    char* buffer = (char*)malloc(maxlen);
+ 
+    if(buffer != NULL) /* NULL if malloc() fails */
+    {
+        int ch = EOF;
+        int pos = 0;
+ 
+        /* Read input one character at a time, resizing the buffer as necessary */
+        while((ch = fgetc(stream)) != '\n' && ch != EOF && !feof(stream))
+        {
+            buffer[pos++] = ch;
+            if(pos == size) /* Next character to be inserted needs more memory */
+            {
+                size = pos + maxlen;
+                buffer = (char*)realloc(buffer, size);
+            }
+        }
+        buffer[pos] = '\0'; /* Null-terminate the completed string */
+    }
+
+    return buffer;
+}
--- include/Fathom.h
+++ include/Fathom.h
@@ -1,3 +1,5 @@
 #include "CClass.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-#define SET_INSTANCE(_TYPE_, _OBJ_) { NEW(_TYPE_, __NEWOBJECT__); _OBJ_ = __NEWOBJECT__; }
+char* ufgets(FILE* stream);
