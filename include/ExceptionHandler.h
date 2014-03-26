f80c6dee43e9a25d0bdffaa4e727746a58f036f1
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-03-25 22:03:53 -0500

    Now uses the ExceptionData class to store the exception handling data.

--- include/ExceptionHandler.h
+++ include/ExceptionHandler.h
@@ -9,6 +9,15 @@
 	{\
 		jmp_buf __FATHOM__jmp_buf;\
 		\
+		ExceptionData* newData = new_ExceptionData(__FATHOM__exception_data, &__FATHOM__jmp_buf, 0, 0);\
+		\
+		if (__FATHOM__exception_data)\
+		{\
+			newData->setParent(newData, __FATHOM__exception_data, __FATHOM__exception_data);\
+		}\
+		\
+		__FATHOM__exception_data = newData;\
+		\
 		switch(setjmp(__FATHOM__jmp_buf))\
 		{\
 			case 0:\
@@ -29,6 +38,6 @@
 	}\
 	while(0)
 
-#define THROW(x) longjmp(__FATHOM__jmp_buf, x)
+#define THROW(x) longjmp(*__FATHOM__exception_data->getBuffer(__FATHOM__exception_data, 0), x)
 
 #endif
