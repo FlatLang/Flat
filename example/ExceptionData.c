40dcc63cf162a0cd7ae158f1d021a3e582a51c0d
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-03-31 22:40:38 -0500

    Updated ExceptionData c output.

--- example/ExceptionData.c
+++ example/ExceptionData.c
@@ -7,14 +7,14 @@
 
 ExceptionData* new_ExceptionData(ExceptionData* __FATHOM__exception_data, jmp_buf* buf);
 void del_ExceptionData(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data);
-static ArrayList* getCodes(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data);
-static void addCode(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code);
-static jmp_buf* getBuffer(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data);
-static ExceptionData* getCorrectData(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code);
-static jmp_buf* getCorrectBuffer(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code);
-static void jumpToBuffer(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code);
-static ExceptionData* getParent(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data);
-static void setParent(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, ExceptionData* p);
+static ArrayList* __FATHOM__getCodes(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data);
+static void __FATHOM__addCode(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code);
+static jmp_buf* __FATHOM__getBuffer(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data);
+static ExceptionData* __FATHOM__getCorrectData(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code);
+static jmp_buf* __FATHOM__getCorrectBuffer(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code);
+static void __FATHOM__jumpToBuffer(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code);
+static ExceptionData* __FATHOM__getParent(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data);
+static void __FATHOM__setParent(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, ExceptionData* p);
 
 PRIVATE
 (
@@ -27,20 +27,22 @@ ExceptionData* new_ExceptionData(ExceptionData* __FATHOM__exception_data, jmp_bu
 {
 NEW(ExceptionData, __o__);
 
-__o__->getCodes = getCodes;
-__o__->addCode = addCode;
-__o__->getBuffer = getBuffer;
-__o__->getCorrectData = getCorrectData;
-__o__->getCorrectBuffer = getCorrectBuffer;
-__o__->jumpToBuffer = jumpToBuffer;
-__o__->getParent = getParent;
-__o__->setParent = setParent;
+__o__->getCodes = __FATHOM__getCodes;
+__o__->addCode = __FATHOM__addCode;
+__o__->getBuffer = __FATHOM__getBuffer;
+__o__->getCorrectData = __FATHOM__getCorrectData;
+__o__->getCorrectBuffer = __FATHOM__getCorrectBuffer;
+__o__->jumpToBuffer = __FATHOM__jumpToBuffer;
+__o__->getParent = __FATHOM__getParent;
+__o__->setParent = __FATHOM__setParent;
 
 __o__->prv->buffer = 0;
 __o__->prv->parent = 0;
 __o__->prv->codes = 0;
+{
 __o__->prv->buffer = buf;
 __o__->prv->codes = new_ArrayList(__FATHOM__exception_data);
+}
 
 return __o__;
 }
@@ -57,41 +59,42 @@ del_ExceptionData(__o__->prv->parent, __FATHOM__exception_data);
 del_ArrayList(__o__->prv->codes, __FATHOM__exception_data);
 free(__o__->prv);
 
+{
+}
 free(__o__);
 }
 
-static ArrayList* getCodes(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data)
+static ArrayList* __FATHOM__getCodes(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data)
 {
 return __o__->prv->codes;
 }
 
-static void addCode(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code)
+static void __FATHOM__addCode(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code)
 {
 __o__->prv->codes->add(__o__->prv->codes, __FATHOM__exception_data, code);
 }
 
-static jmp_buf* getBuffer(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data)
+static jmp_buf* __FATHOM__getBuffer(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data)
 {
 return __o__->prv->buffer;
 }
 
-static ExceptionData* getCorrectData(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code)
+static ExceptionData* __FATHOM__getCorrectData(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code)
 {
 ExceptionData* data;
-ArrayList* list;
-int i;
 
 data = __o__;
 while (data != 0)
 {
+ArrayList* list;
+int i;
+
 list = data->getCodes(data, __FATHOM__exception_data);
-{
-for (i = 0; i < list->getSize(list, __FATHOM__exception_data); i = i + 1)
+for (i = 0; i < list->getSize(list, __FATHOM__exception_data); i++)
 {
 if (list->get(list, __FATHOM__exception_data, i) == code)
 {
 return data;
-}
 }
 }
 if (data->getParent(data, __FATHOM__exception_data) == 0)
@@ -103,11 +106,11 @@ data = data->getParent(data, __FATHOM__exception_data);
 return 0;
 }
 
-static jmp_buf* getCorrectBuffer(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code)
+static jmp_buf* __FATHOM__getCorrectBuffer(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code)
 {
 ExceptionData* data;
 
-data = getCorrectData(__o__, __FATHOM__exception_data, code);
+data = __o__->getCorrectData(__o__, __FATHOM__exception_data, code);
 if (data == 0)
 {
 return 0;
@@ -115,12 +118,12 @@ return 0;
 return data->getBuffer(data, __FATHOM__exception_data);
 }
 
-static void jumpToBuffer(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code)
+static void __FATHOM__jumpToBuffer(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, int code)
 {
 ExceptionData* data;
 jmp_buf* buf;
 
-data = getCorrectData(__o__, __FATHOM__exception_data, code);
+data = __o__->getCorrectData(__o__, __FATHOM__exception_data, code);
 if (data->getParent(data, __FATHOM__exception_data) == 0)
 {
 code = 1;
@@ -129,12 +132,12 @@ buf = data->getBuffer(data, __FATHOM__exception_data);
 longjmp(*buf, code);
 }
 
-static ExceptionData* getParent(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data)
+static ExceptionData* __FATHOM__getParent(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data)
 {
 return __o__->prv->parent;
 }
 
-static void setParent(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, ExceptionData* p)
+static void __FATHOM__setParent(ExceptionData* __o__, ExceptionData* __FATHOM__exception_data, ExceptionData* p)
 {
 __o__->prv->parent = p;
 }
