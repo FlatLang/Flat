8257261467723fbe700e3f67116dc06ee339a035
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-04-29 19:38:08 -0500

    New thread test executable.

--- include/test.c
+++ include/test.c
@@ -1,15 +1,15 @@
 #include "Thread.h"
+#include "Fathom.h"
 #include <stdio.h>
+#include <math.h>
 
 FATHOM_THREAD_FUNC func(FATHOM_THREAD_FUNC_ARG arg)
 {
 	int i;
 	
-	for (i = 0; i < 50; i++)
+	for (i = 0; i < 1000000000 / 2; i++)
 	{
-		printf("i is now: %d\n", i);
-		
-		lib_fathom_thread_sleep(100);
+		sin(i);
 	}
 
 	return 0;
@@ -17,13 +17,21 @@ FATHOM_THREAD_FUNC func(FATHOM_THREAD_FUNC_ARG arg)
 
 int main()
 {
+	long_long start, end;
+	
 	FATHOM_THREAD_HANDLE handle, handle2;
 	
 	lib_fathom_thread_create(&handle, func);
 	lib_fathom_thread_create(&handle2, func);
 	
+	start = currentTimeMillis();
+	
 	lib_fathom_thread_join(handle);
 	lib_fathom_thread_join(handle2);
+	
+	end = currentTimeMillis();
+	
+	printf("Time: %llu\n", (end - start));
 	
 	return 0;
 }
