d1f9323ee7409931e4f62399ab138888a6439fae
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-05-24 23:38:09 -0500

    Moved arrayDimensions attribute into this class. New getContextNode() methods. New getLastAccessedNode() method. New getAccessedNode() method. Documented isContainingClass() method. Moved isPrimitive(), isArray(), getArrayText(), setArrayDimensions() methods into this class. New generateArgumentReference() method. Documented generateChildrenCSourceFragment() method. New generateChildrenCSourceFragment() overloads. Documented generateSpecialFragment() and isSpecialFragment() methods. generateUseOutput() method now outputs the type of the ValueNode. New generateNovaInput() method. Updated cloneTo() method.

--- src/net/fathomsoft/nova/tree/ValueNode.java
+++ src/net/fathomsoft/nova/tree/ValueNode.java
@@ -19,7 +19,9 @@ package net.fathomsoft.nova.tree;
 
 import net.fathomsoft.nova.tree.variables.ArrayAccessNode;
 import net.fathomsoft.nova.tree.variables.ArrayNode;
+import net.fathomsoft.nova.tree.variables.FieldNode;
 import net.fathomsoft.nova.tree.variables.LocalVariableNode;
+import net.fathomsoft.nova.tree.variables.VariableNode;
 import net.fathomsoft.nova.util.SyntaxUtils;
 
 /**
@@ -29,10 +31,12 @@ import net.fathomsoft.nova.util.SyntaxUtils;
  * 
  * @author	Braden Steffaniak
  * @since	v0.2.4 May 2, 2014 at 11:14:37 PM
- * @version	v0.2.4 May 17, 2014 at 9:55:04 PM
+ * @version	v0.2.6 May 24, 2014 at 6:06:20 PM
  */
 public class ValueNode extends TreeNode
 {
+	private int		arrayDimensions;
+	
 	private String	type;
 	
 	/**
@@ -73,7 +77,18 @@ public class ValueNode extends TreeNode
 		}
 		if (parent instanceof InstantiationNode)
 		{
-			parent = parent.getParent();
+			InstantiationNode instantiation = (InstantiationNode)parent;
+			
+			IdentifierNode    identifier    = instantiation.getIdentifierNode();
+			
+			if (this == identifier || identifier instanceof ArrayNode)
+			{
+				parent = parent.getParent();
+			}
+			else
+			{
+				return identifier;
+			}
 		}
 		if (parent instanceof ReturnNode)
 		{
@@ -118,6 +133,98 @@ public class ValueNode extends TreeNode
 		}
 		
 		return (ValueNode)parent;
+	}
+	
+	/**
+	 * Get the furthest node that is accessing the specified node.<br>
+	 * <br>
+	 * For example:
+	 * <blockquote><pre>
+	 * tree.next.calculateSize()</pre></blockquote>
+	 * In the previous statement, "<code>tree</code>" is the context.
+	 * 
+	 * @return The furthest node that accesses the specified node.
+	 */
+	public ValueNode getContextNode()
+	{
+		return getContextNode(getReferenceNode());
+	}
+	
+	/**
+	 * Get the furthest node that is accessing the specified node.<br>
+	 * <br>
+	 * For example:
+	 * <blockquote><pre>
+	 * tree.next.calculateSize()</pre></blockquote>
+	 * In the previous statement, "<code>tree</code>" is the context.
+	 * 
+	 * @param parent The parent of the ValueNode to use.
+	 * @return The furthest node that accesses the specified node.
+	 */
+	public ValueNode getContextNode(ValueNode parent)
+	{
+		TreeNode next = parent;
+		
+		while (next instanceof LocalVariableNode || next instanceof FieldNode)
+		{
+			if (next instanceof LocalVariableNode == false && next instanceof FieldNode == false)//next.containsScope() || next instanceof ReturnNode)
+			{
+				return parent;
+			}
+			
+			parent = (ValueNode)next;
+			next   = parent.getParent();
+		}
+		
+		return parent;
+	}
+	
+	/**
+	 * Get the last node that is accessed by the specified node.<br>
+	 * <br>
+	 * For example:
+	 * <blockquote><pre>
+	 * tree.next.calculateSize()</pre></blockquote>
+	 * In the previous statement, "<code>calculateSize()</code>" is the
+	 * last accessed node.
+	 * 
+	 * @return The last node that is accessed by the specified node.
+	 */
+	public ValueNode getLastAccessedNode()
+	{
+		ValueNode prev = this;
+		ValueNode node = getAccessedNode();
+		
+		while (node != null)
+		{
+			prev = node;
+			
+			node = node.getAccessedNode();
+		}
+		
+		return prev;
+	}
+	
+	/**
+	 * Get the next node that is accessed by the specified node.<br>
+	 * <br>
+	 * For example:
+	 * <blockquote><pre>
+	 * tree.next.calculateSize()</pre></blockquote>
+	 * In the previous statement, "<code>calculateSize()</code>" is the
+	 * accessed node of the "<code>next</code>" node, and "<code>next</code>"
+	 * is the accessed node of the "<code>tree</code>" node.
+	 * 
+	 * @return The next node that is accessed by the specified node.
+	 */
+	public ValueNode getAccessedNode()
+	{
+		if (getChildren().size() <= 0)
+		{
+			return null;
+		}
+		
+		return (ValueNode)getChild(0);
 	}
 	
 	/**
@@ -176,6 +283,14 @@ public class ValueNode extends TreeNode
 		return (ClassNode)getAncestorOfType(ClassNode.class, true);
 	}
 	
+	/**
+	 * Check whether or not the given value is accessed within its direct
+	 * parent class.
+	 * 
+	 * @param The node to check.
+	 * @return Whether or not the node was accessed through its parent
+	 * 		class.
+	 */
 	public boolean isContainingClass(ValueNode node)
 	{
 		ClassNode clazz = node.getClassNode();
@@ -212,6 +327,70 @@ public class ValueNode extends TreeNode
 	public boolean isPrimitiveType()
 	{
 		return SyntaxUtils.isPrimitiveType(getType());
+	}
+	
+	/**
+	 * Get whether a value is primitive or not. A value is primitive
+	 * if it has a primitive type AND is NOT an array. Arrays are NOT a
+	 * primitive type.<br>
+	 * <br>
+	 * For the list of primitive values, see
+	 * {@link net.fathomsoft.nova.util.SyntaxUtils#isPrimitiveType(String)}.
+	 * 
+	 * @return Whether a variable is primitive or not.
+	 */
+	public boolean isPrimitive()
+	{
+		return isPrimitiveType() && !isArray();
+	}
+	
+	/**
+	 * Get whether or not the variable is an array. A variable is an
+	 * array if it has an array dimension of 1 or greater.
+	 * 
+	 * @return Whether or not the variable is an array.
+	 */
+	public boolean isArray()
+	{
+		return getArrayDimensions() > 0;
+	}
+	
+	/**
+	 * Get the amount of dimensions that the array has, if any. For an
+	 * example of what a array declarations and dimensions look like
+	 * {@link #setArrayDimensions(int)}
+	 * 
+	 * @return The amount of dimensions that the array has, if any.
+	 */
+	public int getArrayDimensions()
+	{
+		return arrayDimensions;
+	}
+	
+	/**
+	 * The text that represents an array in the C language.
+	 * 
+	 * @return The text that represents an array in the C language.
+	 */
+	public String getArrayText()
+	{
+		return "*";
+	}
+	
+	/**
+	 * Set the amount of dimensions that the array has, if any.<br>
+	 * <br>
+	 * For example:
+	 * <blockquote><pre>
+	 * int array[][][];</pre></blockquote>
+	 * In the previous example, the variable "array" has three dimensions.
+	 * 
+	 * @param arrayDimensions The amount of dimensions that the array has,
+	 * 		if any.
+	 */
+	public void setArrayDimensions(int arrayDimensions)
+	{
+		this.arrayDimensions = arrayDimensions;
 	}
 	
 	/**
@@ -255,6 +434,18 @@ public class ValueNode extends TreeNode
 	}
 	
 	/**
+	 * Generate the C output for when this value node is being used
+	 * as an argument for a method call.
+	 * 
+	 * @return The C output for when this value node is being used
+	 * 		as an argument for a method call.
+	 */
+	public String generateArgumentReference()
+	{
+		return generateUseOutput() + generateChildrenCSourceFragment(true, true);
+	}
+	
+	/**
 	 * @see net.fathomsoft.nova.tree.TreeNode#generateCSourceFragment()
 	 */
 	@Override
@@ -263,31 +454,93 @@ public class ValueNode extends TreeNode
 		return generateChildrenCSourceFragment(false);
 	}
 	
+	/**
+	 * Generate a String representing the accessed nodes.
+	 * 
+	 * @return The generated String.
+	 */
 	public String generateChildrenCSourceFragment()
 	{
 		return generateChildrenCSourceFragment(true);
 	}
 	
+	/**
+	 * Generate a String representing the accessed nodes.
+	 * 
+	 * @param reference Whether or not to start the string off with
+	 * 		a "->" reference operator.
+	 * @return The generated String.
+	 */
 	public String generateChildrenCSourceFragment(boolean reference)
+	{
+		return generateChildrenCSourceFragment(reference, false);
+	}
+	
+	/**
+	 * Generate a String representing the accessed nodes.
+	 * 
+	 * @param reference Whether or not to start the string off with
+	 * 		a "->" reference operator.
+	 * @param argument Whether or not the specified node is being
+	 * 		output as an argument to a method call.
+	 * @return The generated String.
+	 */
+	public String generateChildrenCSourceFragment(boolean reference, boolean argument)
 	{
 		StringBuilder builder = new StringBuilder();
 		
-		for (int i = 0; i < getChildren().size(); i++)
+		if (getChildren().size() <= 0)
 		{
-			TreeNode child = getChild(i);
+			return "";
+		}
+		
+		TreeNode child = getChild(0);
+		
+		if (argument && (child instanceof MethodCallNode || child instanceof InstantiationNode))
+		{
+			return "";
+		}
+		
+		if (reference)
+		{
+			builder.append("->");
+		}
+		
+		if (child instanceof ValueNode)
+		{
+			ValueNode value = (ValueNode)child;
 			
-			if (i > 0 || reference)
+			if (value.isSpecialFragment())
 			{
-				builder.append("->");
+				builder.append(value.generateUseOutput() + value.generateChildrenCSourceFragment(true, true));
+				
+				return builder.toString();
 			}
-			
+		}
+		
+		String s = child.generateCSourceFragment();
+		
+		if (s != null)
+		{
 			builder.append(child.generateCSourceFragment());
+		}
+		else
+		{
+			if (reference)
+			{
+				builder.delete(builder.length() - 2, builder.length());
+			}
 		}
 		
 		return builder.toString();
 	}
 	
-
+	/**
+	 * If the ValueNode accesses a method call, generate a specialized
+	 * output.
+	 * 
+	 * @return A specialized String generation.
+	 */
 	public String generateSpecialFragment()
 	{
 		for (int i = getChildren().size() - 1; i >= 0; i--)
@@ -298,20 +551,43 @@ public class ValueNode extends TreeNode
 			{
 				return child.generateCSourceFragment();
 			}
+			else if (child instanceof ValueNode)
+			{
+				ValueNode value = (ValueNode)child;
+				
+				if (value.isSpecialFragment())
+				{
+					return value.generateSpecialFragment();
+				}
+			}
 		}
 		
 		return null;
 	}
 	
+	/**
+	 * Get whether or not the ValueNode accesses a method call.
+	 * 
+	 * @return Whether or not the ValueNode accesses a method call.
+	 */
 	public boolean isSpecialFragment()
 	{
-		for (int i = 0; i < getChildren().size(); i++)
+		for (int i = getChildren().size() - 1; i >= 0; i--)
 		{
 			TreeNode child = getChild(i);
 			
 			if (child instanceof MethodCallNode || child instanceof InstantiationNode)
 			{
 				return true;
+			}
+			else if (child instanceof ValueNode)
+			{
+				ValueNode value = (ValueNode)child;
+				
+				if (value.isSpecialFragment())
+				{
+					return true;
+				}
 			}
 		}
 		
@@ -327,7 +603,30 @@ public class ValueNode extends TreeNode
 	 */
 	public String generateUseOutput()
 	{
-		return null;
+		return getType();
+	}
+	
+	/**
+	 * @see net.fathomsoft.nova.tree.TreeNode#generateNovaInput()
+	 */
+	@Override
+	public String generateNovaInput()
+	{
+//		if (isSpecialFragment())
+//		{
+//			return getChild(0).generateNovaInput();
+//		}
+		
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append(generateUseOutput());
+		
+		if (getChildren().size() > 0)
+		{
+			builder.append('.').append(getChild(0).generateNovaInput());
+		}
+		
+		return builder.toString();
 	}
 	
 	/**
@@ -351,7 +650,8 @@ public class ValueNode extends TreeNode
 	public ValueNode cloneTo(ValueNode node)
 	{
 		super.cloneTo(node);
-		
+
+		node.arrayDimensions = arrayDimensions;
 		node.type = type;
 		
 		return node;
