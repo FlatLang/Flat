4aa24d14dc43f14a64307e96f23593f33105ce67
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-06-07 21:43:22 -0500

    Fixed macro overloading for Visual C++ compiler. (Which is crappy)

--- include/CClass.h
+++ include/CClass.h
@@ -24,58 +24,65 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define GET_MACRO2P(_1_, _2_, NAME, ...) NAME
-#define GET_MACRO3P(_1_, _2_, _3_, NAME, ...) NAME
-#define GET_MACRO4P(_1_, _2_, _3_, _4_, NAME, ...) NAME
-#define GET_MACRO5P(_1_, _2_, _3_, _4_, _5_, NAME, ...) NAME
+//#define TCC
 
-#define CCLASS_NEW(...) GET_MACRO3P(__VA_ARGS__, CCLASS_NEW4, CCLASS_NEW3)(__VA_ARGS__)
-
-#define CCLASS_NEW3(_CLASS_, _OBJ_) CCLASS_NEW4(_CLASS_, _OBJ_, _OBJ_->prv = new_Private())
-
-#define CCLASS_NEW4(_CCLASS_CLASS_, _OBJ_, _CCLASS_PRIVATE_)\
-	_CCLASS_CLASS_* _OBJ_ = CCLASS_NEW2(_CCLASS_CLASS_);\
+#define CCLASS_NEW1(_CLASS_) (_CLASS_*)GC_MALLOC(sizeof(_CLASS_))
+#define CCLASS_NEW3(_CLASS_, _OBJ_, _CCLASS_PRIVATE_)\
+	_CLASS_* _OBJ_ = CCLASS_NEW1(_CLASS_);\
 	_CCLASS_PRIVATE_
+#define CCLASS_NEW2(_CLASS_, _OBJ_) CCLASS_NEW3(_CLASS_, _OBJ_, _OBJ_->prv = new_Private())
 
-#define CCLASS_NEW2(_CLASS_) (_CLASS_*)GC_MALLOC(sizeof(_CLASS_))
-
-#define CCLASS_NEW_EXT(_CLASS_, _EXTENDS_, _OBJ_) CCLASS_NEW(_CLASS_, _OBJ_); _OBJ_->getParent = getParent
-
-#define CCLASS_DEL(_OBJ_) CCLASS_DELETE(_OBJ_->prv); CCLASS_DELETE(_OBJ_)
-
-#define CCLASS_DELETE(_OBJ_) _OBJ_ = NULL//free(_OBJ_)
-
-#define CCLASS_FUNC(_TYPE_, _NAME_, ...) _TYPE_ (*_NAME_)(__VA_ARGS__)
-
-#define CCLASS_CLASS(...) GET_MACRO3P(__VA_ARGS__, CCLASS_CLASS3, CCLASS_CLASS2, CCLASS_CLASS1)(__VA_ARGS__);
-
+#define CCLASS_CLASS_DEF(_NAME_, _BODY_) struct _NAME_ { _BODY_ }
+//#define CCLASS_CLASS3(_NAME_, _BODY_) typedef struct _NAME_ { _BODY_ } _NAME_;
+#define CCLASS_CLASS2(_NAME_, _BODY_) CCLASS_CLASS_DEF(_NAME_, _BODY_)
 #define CCLASS_CLASS1(_NAME_) CCLASS_CLASS2(_NAME_,)
 
-#define CCLASS_CLASS2(_NAME_, _BODY_)\
-	CCLASS_CLASS_DEF(_NAME_, _BODY_)\
-	//CCLASS_DECLARE_METHODS(_NAME_, _METHOD_PREFIX_)
 
-#define CCLASS_CLASS4(_NAME_, _BODY_, _METHOD_PREFIX_)\
-	typedef struct _NAME_\
+#ifdef TCC
+#	define GET_MACRO2P(_1_, _2_, NAME, ...) NAME
+#	define GET_MACRO3P(_1_, _2_, _3_, NAME, ...) NAME
+#	define GET_MACRO4P(_1_, _2_, _3_, _4_, NAME, ...) NAME
+#	define GET_MACRO5P(_1_, _2_, _3_, _4_, _5_, NAME, ...) NAME
+	
+#	define CCLASS_NEW(...) GET_MACRO3P(__VA_ARGS__, CCLASS_NEW3, CCLASS_NEW2, CCLASS_NEW1)(__VA_ARGS__)
+#	define CCLASS_CLASS(...) GET_MACRO2P(__VA_ARGS__, CCLASS_CLASS2, CCLASS_CLASS1)(__VA_ARGS__);
+#else
+#	define GLUE(x, y) x y
+
+#	define RETURN_ARG_COUNT(_1_, _2_, _3_, _4_, _5_, count, ...) count
+#	define EXPAND_ARGS(args) RETURN_ARG_COUNT args
+#	define COUNT_ARGS_MAX5(...) EXPAND_ARGS((__VA_ARGS__, 5, 4, 3, 2, 1, 0))
+
+#	define OVERLOAD_MACRO2(name, count) name##count
+#	define OVERLOAD_MACRO1(name, count) OVERLOAD_MACRO2(name, count)
+#	define OVERLOAD_MACRO(name, count) OVERLOAD_MACRO1(name, count)
+
+#	define CALL_OVERLOAD(name, ...) GLUE(OVERLOAD_MACRO(name, COUNT_ARGS_MAX5(__VA_ARGS__)), (__VA_ARGS__));
+
+#	define CCLASS_NEW(...) CALL_OVERLOAD(CCLASS_NEW, __VA_ARGS__)
+#	define CCLASS_CLASS(...) CALL_OVERLOAD(CCLASS_CLASS, __VA_ARGS__);
+#endif
+
+
+#define CCLASS_DELETE(_OBJ_) free(_OBJ_)
+#define CCLASS_DEL(_OBJ_) CCLASS_DELETE(_OBJ_->prv); CCLASS_DELETE(_OBJ_)
+#define CCLASS_FUNC(_TYPE_, _NAME_, ...) _TYPE_ (*_NAME_)(__VA_ARGS__)
+
+#define CCLASS_INSTANTIATE(_CLASS_, _OBJ_, _VARIABLE_INITIALIZE_)\
+	do\
 	{\
-		_BODY_\
-	} _NAME_;\
-	//\
-	//CCLASS_DECLARE_METHODS(_NAME_, _METHOD_PREFIX_)
-
-#define CCLASS_CLASS_DEF(_NAME_, _BODY_)\
-	/*typedef struct _NAME_ _NAME_;*/\
-	struct _NAME_\
-	{\
-		_BODY_\
-	};
-
-#define CCLASS_DECLARE_METHODS(_CLASS_, _METHOD_PREFIX_)\
-	_METHOD_PREFIX_ _CLASS_* new_##_CLASS_();\
-	_METHOD_PREFIX_ void del_##_CLASS_(_CLASS_* obj)
+		_CLASS_* _OBJ_ = CCLASS_NEW1(_CLASS_);\
+		assert(_OBJ_);\
+		\
+		{\
+			_VARIABLE_INITIALIZE_;\
+		}\
+		\
+		return _OBJ_;\
+	} while (0)
 
 #define CCLASS_PRIVATE(_CCLASS_PRIVATE_VARIABLES_)\
-	CCLASS_CLASS4(Private, _CCLASS_PRIVATE_VARIABLES_, static);\
+	typedef struct Private { _CCLASS_PRIVATE_VARIABLES_ } Private;\
 	\
 	static Private* new_Private()\
 	{\
@@ -85,18 +92,5 @@
 	{\
 		CCLASS_DELETE(p);\
 	}
-
-#define CCLASS_INSTANTIATE(_CLASS_, _OBJ_, _VARIABLE_INITIALIZE_)\
-	do\
-	{\
-		_CLASS_* _OBJ_ = CCLASS_NEW2(_CLASS_);\
-		assert(_OBJ_);\
-		\
-		{\
-			_VARIABLE_INITIALIZE_;\
-		}\
-		\
-		return _OBJ_;\
-	} while (0)
 
 #endif
