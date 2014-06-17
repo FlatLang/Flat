3570a2c96358ce859e98bcad6cbf4c58d28d942c
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-06-17 13:29:06 -0500

    Updated clone() method documentation.

--- src/net/fathomsoft/nova/tree/InstanceDeclarationNode.java
+++ src/net/fathomsoft/nova/tree/InstanceDeclarationNode.java
@@ -10,7 +10,7 @@ import net.fathomsoft.nova.util.Location;
  * 
  * @author	Braden Steffaniak
  * @since	v0.1 Jan 5, 2014 at 9:10:49 PM
- * @version	v0.2.7 May 25, 2014 at 9:16:48 PM
+ * @version	v0.2.13 Jun 17, 2014 at 8:45:35 AM
  */
 public class InstanceDeclarationNode extends VariableNode
 {
@@ -307,7 +307,7 @@ public class InstanceDeclarationNode extends VariableNode
 	}
 	
 	/**
-	 * @see net.fathomsoft.nova.tree.TreeNode#clone(TreeNode)
+	 * @see net.fathomsoft.nova.tree.TreeNode#clone(TreeNode, Location)
 	 */
 	@Override
 	public InstanceDeclarationNode clone(TreeNode temporaryParent, Location locationIn)
