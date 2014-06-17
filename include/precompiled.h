fed7e981eef953c1286ad459f8686dd06ccf32f9
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-06-17 16:38:14 -0500

    New precompiled header file to help speed up compilation.

---
+++ include/precompiled.h
@@ -0,0 +1,27 @@
+#ifndef NOVA_PRECOMPILED
+#define NOVA_PRECOMPILED
+
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/timeb.h>
+
+#include <assert.h>
+#include <CClass.h>
+
+#ifdef _WIN32
+#	include <windows.h>
+#elif defined(__APPLE__) || defined(__linux__)
+#	include <pthread.h>
+#	include <unistd.h>
+#endif
+
+#ifdef USE_GC
+#	include <gc.h>
+#endif
+
+#endif
