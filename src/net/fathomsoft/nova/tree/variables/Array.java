6cbc3e9f1884c0f56268a3745544e4aafb15ca2d
Braden Steffaniak <BradenSteffaniak@gmail.com>
2014-10-02 17:59:59 -0600

    Now implements ArrayCompatible. Now accepts array initializer as input.

--- src/net/fathomsoft/nova/tree/variables/Array.java
+++ src/net/fathomsoft/nova/tree/variables/Array.java
@@ -2,12 +2,16 @@ package net.fathomsoft.nova.tree.variables;
 
 import net.fathomsoft.nova.TestContext;
 import net.fathomsoft.nova.error.SyntaxMessage;
+import net.fathomsoft.nova.tree.Assignment;
 import net.fathomsoft.nova.tree.BinaryOperation;
+import net.fathomsoft.nova.tree.Dimensions;
 import net.fathomsoft.nova.tree.Identifier;
 import net.fathomsoft.nova.tree.Literal;
 import net.fathomsoft.nova.tree.Node;
 import net.fathomsoft.nova.tree.Priority;
 import net.fathomsoft.nova.tree.SyntaxTree;
+import net.fathomsoft.nova.tree.TypeList;
+import net.fathomsoft.nova.tree.Value;
 import net.fathomsoft.nova.util.Location;
 import net.fathomsoft.nova.util.StringUtils;
 import net.fathomsoft.nova.util.SyntaxUtils;
@@ -29,7 +33,7 @@ import net.fathomsoft.nova.util.SyntaxUtils;
  * @since	v0.1 Mar 16, 2014 at 1:13:49 AM
  * @version	v0.2.33 Sep 29, 2014 at 10:29:33 AM
  */
-public class Array extends VariableDeclaration
+public class Array extends VariableDeclaration implements ArrayCompatible
 {
 	/**
 	 * @see net.fathomsoft.nova.tree.Node#Node(Node, Location)
@@ -37,6 +41,34 @@ public class Array extends VariableDeclaration
 	public Array(Node temporaryParent, Location locationIn)
 	{
 		super(temporaryParent, locationIn);
+		
+		Dimensions dimensions = new Dimensions(this, locationIn);
+		
+		addChild(dimensions);
+	}
+	
+	@Override
+	public int getNumDefaultChildren()
+	{
+		return super.getNumDefaultChildren() + 1 + getChildrenOffset();
+	}
+	
+	private int getChildrenOffset()
+	{
+		int offset = 0;
+		
+		if (getInitializerValues() != null)
+		{
+			offset++;
+		}
+		
+		return offset;
+	}
+	
+	@Override
+	public Dimensions getDimensions()
+	{
+		return (Dimensions)getChild(super.getNumDefaultChildren() + 0);
 	}
 	
 	/**
@@ -45,12 +77,12 @@ public class Array extends VariableDeclaration
 	@Override
 	public Identifier getAccessedNode()
 	{
-		if (getNumVisibleChildren() <= getArrayDimensions())
+		if (getNumVisibleChildren() <= super.getNumDefaultChildren() + getChildrenOffset() + 1)
 		{
 			return null;
 		}
 		
-		return (Identifier)getVisibleChild(getArrayDimensions());
+		return (Identifier)getVisibleChild(super.getNumDefaultChildren() + getChildrenOffset() + 1);
 	}
 	
 	/**
@@ -60,6 +92,17 @@ public class Array extends VariableDeclaration
 	public int getNumDecodedChildren()
 	{
 		return super.getNumDecodedChildren() + 1;
+	}
+	
+	public TypeList<Value> getInitializerValues()
+	{
+		if (getNumChildren() <= super.getNumDefaultChildren() + 1 ||
+				!(getChild(super.getNumDefaultChildren() + 1) instanceof TypeList))
+		{
+			return null;
+		}
+		
+		return (TypeList<Value>)getChild(super.getNumDefaultChildren() + 1);
 	}
 	
 	/**
@@ -80,38 +123,34 @@ public class Array extends VariableDeclaration
 		generateCTypeCast(builder);
 //		builder.insert(builder.length() - 1, '*');
 		
-		if (getNumChildren() > 1)
+		if (getNumDimensions() > 1)
 		{
 			builder.append("nova_gen_array(");
 		}
 		
 		builder.append("NOVA_MALLOC(sizeof(").append(generateCTypeClassName());
 		
-		for (int i = 0; i < getNumChildren(); i++)
-		{
-			builder.append('[');
-			getChild(i).generateCSourceFragment(builder);
-			builder.append(']');
-		}
+		Dimensions dim = getDimensions();
+		
+		dim.generateCSourceFragment(builder);
 		
 		builder.append(')');
 		
-		if (getNumChildren() > 1)
+		if (getNumDimensions() > 1)
 		{
-//			l1_Novas = (nova_standard_NovaString****)nova_gen_array(NOVA_MALLOC(sizeof(nova_standard_NovaString[5][3][2])), (int[]) { 3, 2 }, 0, 2, sizeof(nova_standard_NovaString));
 			builder.append("), (int[]) { ");
 			
-			for (int i = 0; i < getNumChildren() - 1; i++)
+			for (int i = 0; i < dim.getNumVisibleChildren(); i++)
 			{
 				if (i > 0)
 				{
 					builder.append(", ");
 				}
 				
-				getChild(i).generateCSourceFragment(builder);
+				dim.getVisibleChild(i).generateCSourceFragment(builder);
 			}
 			
-			builder.append(" }, 0, ").append(getNumChildren() - 1).append(", ");
+			builder.append(" }, 0, ").append(dim.getNumVisibleChildren() - 1).append(", ");
 			
 			builder.append("sizeof(").append(generateCTypeClassName()).append(')');
 		}
@@ -127,10 +166,7 @@ public class Array extends VariableDeclaration
 	{
 		builder.append(getName());
 		
-		for (int i = 0; i < getNumChildren(); i++)
-		{
-			builder.append('[').append(getChild(i).generateNovaInput()).append(']');
-		}
+		getDimensions().generateNovaInput(builder);
 		
 		return builder;
 	}
@@ -163,10 +199,10 @@ public class Array extends VariableDeclaration
 	 */
 	public static Array decodeStatement(Node parent, String statement, Location location, boolean require)
 	{
+		Array n = new Array(parent, location);
+		
 		if (SyntaxUtils.isArrayInitialization(statement))
 		{
-			Array n = new Array(parent, location);
-			
 			int index = statement.indexOf('[');
 			
 			Location newLoc = location.asNew();
@@ -190,8 +226,93 @@ public class Array extends VariableDeclaration
 				return n;
 			}
 		}
+		else if (n.isInitializer(parent, statement, require))
+		{
+			if (n.decodeInitializer(statement, require))
+			{
+				VariableDeclaration declaration = n.getArrayDeclaration();
+				
+				n.setName(declaration.getType());
+				n.setType(n.getName());
+				n.setArrayDimensions(declaration.getArrayDimensions());
+				
+				return n;
+			}
+		}
 		
 		return null;
+	}
+	
+	private boolean isInitializer(Node parent, String statement, boolean require)
+	{
+		if (!(parent instanceof Assignment))
+		{
+			return SyntaxMessage.queryError("Array initializer is only valid during an assignment", this, require);
+		}
+		
+		return isInitializer(statement);
+	}
+	
+	public static boolean isInitializer(String statement)
+	{
+		return getInitializerContents(statement) != null;
+	}
+	
+	private static String getInitializerContents(String statement)
+	{
+		if (statement.length() > 2 && statement.charAt(0) == '[' && statement.charAt(statement.length() - 1) == ']')
+		{
+			return statement.substring(1, statement.length() - 1).trim();
+		}
+		
+		return null;
+	}
+	
+	private VariableDeclaration getArrayDeclaration()
+	{
+		return ((Assignment)getParent()).getAssigneeNode().getDeclaration();
+	}
+	
+	private boolean decodeInitializer(String statement, boolean require)
+	{
+		String contents = getInitializerContents(statement);
+		
+		if (contents == null)
+		{
+			return false;
+		}
+		else if (!getArrayDeclaration().isArray())
+		{
+			return SyntaxMessage.queryError("Array initializer can only be assigned to arrays", this, require);
+		}
+		
+		TypeList<Value> initValues = new TypeList<Value>(this, getLocationIn().asNew());
+		
+		String values[] = StringUtils.splitCommas(contents);
+		
+		for (String value : values)
+		{
+			Value val = SyntaxTree.decodeValue(initValues, value, getLocationIn().asNew(), require);
+			
+			if (val == null)
+			{
+				return SyntaxMessage.queryError("Cannot decode value '" + value + "'", this, require);
+			}
+			else if (!SyntaxUtils.isTypeCompatible(getArrayDeclaration(), val))
+			{
+				return SyntaxMessage.queryError("Type '" + val.getType() + "' is not compatible with array type '" + getArrayDeclaration().getType() + "'", this, require);
+			}
+			
+			initValues.addChild(val);
+		}
+		
+		Literal size = Literal.decodeStatement(this, "" + values.length, getLocationIn(), true);
+		
+		getDimensions().addChild(size);
+		
+		addChild(initValues);
+		
+		return true;
 	}
 	
 	/**
@@ -255,7 +376,32 @@ public class Array extends VariableDeclaration
 			}
 		}
 		
-		addChild(node);
+		getDimensions().addChild(node);
+	}
+	
+	@Override
+	public Node validate(int phase)
+	{
+		if (phase == SyntaxTree.PHASE_METHOD_CONTENTS)
+		{
+			TypeList<Value> initValues = getInitializerValues();
+			
+			if (initValues != null)
+			{
+				Node base = getBaseNode();
+				
+				for (int i = initValues.getNumVisibleChildren() - 1; i >= 0; i--)
+				{
+					Value value = initValues.getVisibleChild(i);
+					
+					Assignment a = Assignment.decodeStatement(this, getArrayDeclaration().getName() + "[" + i + "] = " + value.generateNovaInput(), getLocationIn(), true);
+					
+					base.getParent().addChildAfter(base, a);
+				}
+			}
+		}
+		
+		return super.validate(phase);
 	}
 	
 	/**
