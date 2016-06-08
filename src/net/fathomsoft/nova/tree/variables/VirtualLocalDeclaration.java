27b8d1bc2de79ee1c943003a5ed2a491e8dfa874
Braden Steffaniak <BradenSteffaniak@gmail.com>
2016-06-08 13:14:24 -0500

    New class for fixing virtual declarations when type is not imported.

---
+++ src/net/fathomsoft/nova/tree/variables/VirtualLocalDeclaration.java
@@ -0,0 +1,73 @@
+package net.fathomsoft.nova.tree.variables;
+
+import net.fathomsoft.nova.TestContext;
+import net.fathomsoft.nova.tree.LocalDeclaration;
+import net.fathomsoft.nova.tree.Node;
+import net.fathomsoft.nova.tree.Value;
+import net.fathomsoft.nova.util.Location;
+
+/**
+ * {@link LocalDeclaration} extension that represents
+ * 
+ * @author	Braden Steffaniak
+ */
+public class VirtualLocalDeclaration extends LocalDeclaration
+{
+	private Value reference;
+	
+	/**
+	 * @see net.fathomsoft.nova.tree.Node#Node(Node, Location)
+	 */
+	public VirtualLocalDeclaration(Node temporaryParent, Location locationIn)
+	{
+		super(temporaryParent, locationIn);
+	}
+	
+	public Value getReference()
+	{
+		return reference;
+	}
+	
+	public void setReference(Value reference)
+	{
+		this.reference = reference;
+	}
+	
+	/**
+	 * @see net.fathomsoft.nova.tree.Node#cloneTo(Node)
+	 */
+	public VirtualLocalDeclaration cloneTo(VirtualLocalDeclaration node)
+	{
+		return cloneTo(node, true);
+	}
+	
+	/**
+	 * Fill the given {@link VirtualLocalDeclaration} with the data that is in the
+	 * specified node.
+	 * 
+	 * @param node The node to copy the data into.
+	 * @return The cloned node.
+	 */
+	public VirtualLocalDeclaration cloneTo(VirtualLocalDeclaration node, boolean cloneChildren)
+	{
+		super.cloneTo(node, cloneChildren);
+		
+		node.reference = reference;
+		
+		return node;
+	}
+	
+	/**
+	 * Test the {@link VirtualLocalDeclaration} class type to make sure everything
+	 * is working properly.
+	 * 
+	 * @return The error output, if there was an error. If the test was
+	 * 		successful, null is returned.
+	 */
+	public static String test(TestContext context)
+	{
+		
+		
+		return null;
+	}
+}
