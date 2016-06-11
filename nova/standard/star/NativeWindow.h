3be6e951b52a6f0e833753c037cff4e0d4bc177f
Braden Steffaniak <BradenSteffaniak@gmail.com>
2016-06-11 18:44:27 -0500

    Fixed merge issues.

--- nova/standard/star/NativeWindow.h
+++ nova/standard/star/NativeWindow.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef NOVA_NATIVE_WINDOW
 #define NOVA_NATIVE_WINDOW
 
@@ -17,23 +16,3 @@
 WINDOW_ID_TYPE nova_createWindow(int x, int y, int width, int height, char* title);
 
 #endif
-=======
-#ifndef NOVA_NATIVE_WINDOW
-#define NOVA_NATIVE_WINDOW
-
-#include <Nova.h>
-
-#include <string.h>
-#include <stdlib.h>
-
-#ifdef _WIN32
-#	include <windows.h>
-
-#	define WINDOW_ID_TYPE HWND
-#else
-#   define WINDOW_ID_TYPE void*
-#endif
-WINDOW_ID_TYPE nova_createWindow(int x, int y, int width, int height, char* title);
-
-#endif
->>>>>>> branch 'ExtraordinarilyGeneric' of https://FoxyCorndog@github.com/FoxyCorndog/Nova.git
