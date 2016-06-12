7545da4714961416ca8242bd72067529aed251bf
Braden Steffaniak <BradenSteffaniak@gmail.com>
2016-06-11 20:53:41 -0500

    Now makes sure that LEAN_AND_MEAN is not defined before defining it.

--- include/precompiled.h
+++ include/precompiled.h
@@ -15,7 +15,9 @@
 #	ifdef _MSC_VER
 //#		define _CRT_SECURE_NO_WARNINGS
 #	endif
-#	define WIN32_LEAN_AND_MEAN
+#	ifndef WIN32_LEAN_AND_MEAN
+#		define WIN32_LEAN_AND_MEAN
+#	endif
 #	include <windows.h>
 #else
 #	include <pthread.h>
