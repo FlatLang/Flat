704031d818821d51ffcf32a98558f3293b130ab0
Braden Steffaniak <BradenSteffaniak@gmail.com>
2016-07-15 22:35:30 -0500

    New method declaration type created to execute lambda contents.

---
+++ src/net/fathomsoft/nova/tree/lambda/LambdaMethodDeclaration.java
@@ -0,0 +1,111 @@
+package net.fathomsoft.nova.tree.lambda;
+
+import net.fathomsoft.nova.TestContext;
+import net.fathomsoft.nova.error.SyntaxMessage;
+import net.fathomsoft.nova.tree.BodyMethodDeclaration;
+import net.fathomsoft.nova.tree.Node;
+import net.fathomsoft.nova.tree.Parameter;
+import net.fathomsoft.nova.util.Location;
+
+/**
+ * {@link BodyMethodDeclaration} extension that represents
+ *
+ * @author	Braden Steffaniak
+ */
+public class LambdaMethodDeclaration extends BodyMethodDeclaration
+{
+	private int unnamedParameterPosition = 0;
+	
+	/**
+	 * @see net.fathomsoft.nova.tree.Node#Node(Node, Location)
+	 */
+	public LambdaMethodDeclaration(Node temporaryParent, Location locationIn)
+	{
+		super(temporaryParent, locationIn);
+	}
+	
+	public Parameter getNextUnnamedParameter()
+	{
+		if (getParameterList().getNumParameters() < unnamedParameterPosition)
+		{
+			SyntaxMessage.error("Too many unnamed lambda expression parameters used", this);
+			
+			return null;
+		}
+		
+		return getParameterList().getParameter(unnamedParameterPosition++);
+	}
+	
+	/**
+	 * Decode the given statement into a {@link LambdaMethodDeclaration} instance, if
+	 * possible. If it is not possible, this method returns null.<br>
+	 * <br>
+	 * Example inputs include:<br>
+	 * <ul>
+	 * 	<li></li>
+	 * 	<li></li>
+	 * 	<li></li>
+	 * </ul>
+	 *
+	 * @param parent The parent node of the statement.
+	 * @param statement The statement to try to decode into a
+	 * 		{@link LambdaMethodDeclaration} instance.
+	 * @param location The location of the statement in the source code.
+	 * @param require Whether or not to throw an error if anything goes wrong.
+	 * @return The generated node, if it was possible to translated it
+	 * 		into a {@link LambdaMethodDeclaration}.
+	 */
+	public static LambdaMethodDeclaration decodeStatement(Node parent, String statement, Location location, boolean require)
+	{
+		
+		
+		return null;
+	}
+	
+	/**
+	 * @see net.fathomsoft.nova.tree.Node#clone(Node, Location, boolean)
+	 */
+	@Override
+	public LambdaMethodDeclaration clone(Node temporaryParent, Location locationIn, boolean cloneChildren)
+	{
+		LambdaMethodDeclaration node = new LambdaMethodDeclaration(temporaryParent, locationIn);
+		
+		return cloneTo(node, cloneChildren);
+	}
+	
+	/**
+	 * @see net.fathomsoft.nova.tree.Node#cloneTo(Node)
+	 */
+	public LambdaMethodDeclaration cloneTo(LambdaMethodDeclaration node)
+	{
+		return cloneTo(node, true);
+	}
+	
+	/**
+	 * Fill the given {@link LambdaMethodDeclaration} with the data that is in the
+	 * specified node.
+	 *
+	 * @param node The node to copy the data into.
+	 * @return The cloned node.
+	 */
+	public LambdaMethodDeclaration cloneTo(LambdaMethodDeclaration node, boolean cloneChildren)
+	{
+		super.cloneTo(node, cloneChildren);
+		
+		return node;
+	}
+	
+	/**
+	 * Test the {@link LambdaMethodDeclaration} class type to make sure everything
+	 * is working properly.
+	 *
+	 * @return The error output, if there was an error. If the test was
+	 * 		successful, null is returned.
+	 */
+	public static String test(TestContext context)
+	{
+		
+		
+		return null;
+	}
+}
