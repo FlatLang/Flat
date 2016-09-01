5ea1c0af5703bb187ec3cd06b5be396813c5dd54
BSteffaniak <BradenSteffaniak@gmail.com>
2016-08-31 23:44:13 -0500

    Removed clutter.

--- nova/network/NativeClientSocket.h
+++
@@ -1,8 +0,0 @@
-#ifndef NOVA_NATIVE_CLIENT_SOCKET
-#define NOVA_NATIVE_CLIENT_SOCKET
-
-#include "NativeSocket.h"
-
-SOCKET_ID_TYPE nova_clientsocket_connect(char* ipAddress, int port);
-
-#endif
--- nova/time/NativeTime.h
+++
@@ -1,6 +0,0 @@
-#ifndef NATIVE_TIME
-#define NATIVE_TIME
-
-long_long currentTimeMillis();
-
-#endif
