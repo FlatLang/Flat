28a67ea3e95e5566a806a85f4f6787be8861728b
BSteffaniak <BradenSteffaniak@gmail.com>
2016-11-05 23:17:42 -0700

    Added null safeguard.

--- src/net/fathomsoft/nova/tree/Assignment.java
+++ src/net/fathomsoft/nova/tree/Assignment.java
@@ -481,7 +481,7 @@ public class Assignment extends Value
 				//declaration.setType(assignmentType.getType(), true, false, false);
 				declaration.correspondingImplicit = assignment;
 				
-				if (assignment.isPrimitive() && assignment.getType().equals("Byte") || assignment.getType().equals("Short"))
+				if (assignment.getType() != null && (assignment.isPrimitive() && assignment.getType().equals("Byte") || assignment.getType().equals("Short")))
 				{
 					declaration.setType("Int");
 				}
