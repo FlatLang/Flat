c29a5d561c955fe5f5a5c22df2926b2fa7a3eabd
BSteffaniak <BradenSteffaniak@gmail.com>
2016-11-28 04:46:15 -0800

    Added Visible Annotation implementation.

---
+++ src/net/fathomsoft/nova/tree/annotations/VisibleAnnotation.java
@@ -0,0 +1,72 @@
+package net.fathomsoft.nova.tree.annotations;
+
+import net.fathomsoft.nova.ValidationResult;
+import net.fathomsoft.nova.tree.Node;
+import net.fathomsoft.nova.tree.variables.FieldDeclaration;
+import net.fathomsoft.nova.util.Location;
+
+public class VisibleAnnotation extends ApplicableAnnotationBase
+{
+	public VisibleAnnotation(Node temporaryParent, Location locationIn)
+	{
+		super(temporaryParent, locationIn);
+	}
+	
+	public static VisibleAnnotation decodeStatement(Node parent, String name, String parameters, Location location, boolean require)
+	{
+		if (name.equals("Visible"))
+		{
+			VisibleAnnotation n = new VisibleAnnotation(parent, location);
+			
+			return n;
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public ValidationResult validate(int phase)
+	{
+		ValidationResult result = super.validate(phase);
+		
+		if (result.skipValidation())
+		{
+			return result;
+		}
+		
+		return result;
+	}
+	
+	@Override
+	public boolean onApplied(Node next)
+	{
+		if (next instanceof FieldDeclaration)
+		{
+			((FieldDeclaration)next).setVisibility(FieldDeclaration.VISIBLE);
+			
+			return true;
+		}
+		
+		return super.onApplied(next);
+	}
+	
+	@Override
+	public VisibleAnnotation clone(Node temporaryParent, Location locationIn, boolean cloneChildren)
+	{
+		VisibleAnnotation node = new VisibleAnnotation(temporaryParent, locationIn);
+		
+		return cloneTo(node, cloneChildren);
+	}
+	
+	public VisibleAnnotation cloneTo(VisibleAnnotation node)
+	{
+		return cloneTo(node, true);
+	}
+	
+	public VisibleAnnotation cloneTo(VisibleAnnotation node, boolean cloneChildren)
+	{
+		super.cloneTo(node, cloneChildren);
+		
+		return node;
+	}
+}
