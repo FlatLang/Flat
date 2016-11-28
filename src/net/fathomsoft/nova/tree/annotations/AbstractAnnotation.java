1487f81bd1cc3298b0e97f2eb3f577f753ed82e6
BSteffaniak <BradenSteffaniak@gmail.com>
2016-11-28 04:42:22 -0800

    Added Abstract Annotation implementation.

---
+++ src/net/fathomsoft/nova/tree/annotations/AbstractAnnotation.java
@@ -0,0 +1,79 @@
+package net.fathomsoft.nova.tree.annotations;
+
+import net.fathomsoft.nova.ValidationResult;
+import net.fathomsoft.nova.tree.ClassDeclaration;
+import net.fathomsoft.nova.tree.Node;
+import net.fathomsoft.nova.tree.NovaMethodDeclaration;
+import net.fathomsoft.nova.util.Location;
+
+public class AbstractAnnotation extends ApplicableAnnotationBase
+{
+	public AbstractAnnotation(Node temporaryParent, Location locationIn)
+	{
+		super(temporaryParent, locationIn);
+	}
+	
+	public static AbstractAnnotation decodeStatement(Node parent, String name, String parameters, Location location, boolean require)
+	{
+		if (name.equals("Abstract"))
+		{
+			AbstractAnnotation n = new AbstractAnnotation(parent, location);
+			
+			return n;
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public ValidationResult validate(int phase)
+	{
+		ValidationResult result = super.validate(phase);
+		
+		if (result.skipValidation())
+		{
+			return result;
+		}
+		
+		return result;
+	}
+	
+	@Override
+	public boolean onApplied(Node next)
+	{
+		if (next instanceof ClassDeclaration)
+		{
+			((ClassDeclaration)next).abstractValue = true;
+		}
+		else if (next instanceof NovaMethodDeclaration)
+		{
+			// Abstract method parser will detect this annotation
+		}
+		else
+		{
+			return super.onApplied(next);
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public AbstractAnnotation clone(Node temporaryParent, Location locationIn, boolean cloneChildren)
+	{
+		AbstractAnnotation node = new AbstractAnnotation(temporaryParent, locationIn);
+		
+		return cloneTo(node, cloneChildren);
+	}
+	
+	public AbstractAnnotation cloneTo(AbstractAnnotation node)
+	{
+		return cloneTo(node, true);
+	}
+	
+	public AbstractAnnotation cloneTo(AbstractAnnotation node, boolean cloneChildren)
+	{
+		super.cloneTo(node, cloneChildren);
+		
+		return node;
+	}
+}
