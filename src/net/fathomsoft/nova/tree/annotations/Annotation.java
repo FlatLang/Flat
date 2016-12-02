38da351865460a4269b3c74871b601602edc255e
BSteffaniak <BradenSteffaniak@gmail.com>
2016-12-01 16:19:14 -0800

    Added generic parameter parsing.

--- src/net/fathomsoft/nova/tree/annotations/Annotation.java
+++ src/net/fathomsoft/nova/tree/annotations/Annotation.java
@@ -5,8 +5,17 @@ import net.fathomsoft.nova.ValidationResult;
 import net.fathomsoft.nova.error.SyntaxErrorException;
 import net.fathomsoft.nova.error.SyntaxMessage;
 import net.fathomsoft.nova.tree.Node;
+import net.fathomsoft.nova.tree.SyntaxTree;
+import net.fathomsoft.nova.tree.Value;
 import net.fathomsoft.nova.util.Location;
 import net.fathomsoft.nova.util.StringUtils;
+import net.fathomsoft.nova.util.SyntaxUtils;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * {@link Node} extension that represents
@@ -15,12 +24,154 @@ import net.fathomsoft.nova.util.StringUtils;
  */
 public class Annotation extends Node
 {
+	public HashMap<String, Object> parameters = new HashMap<>();
+	
+	public static final HashMap<String, Constructor> MODIFIERS = new HashMap<>();
+	
+	static
+	{
+		Class<?>[] classes = new Class<?>[] {
+			AbstractAnnotation.class,
+			NativeAnnotation.class,
+			ObsoleteAnnotation.class,
+			OverrideAnnotation.class,
+			PrivateAnnotation.class,
+			PublicAnnotation.class,
+			StaticAnnotation.class,
+			VisibleAnnotation.class,
+			ImmutableAnnotation.class
+		};
+		
+		Arrays.stream(classes).forEach(c -> {
+			try
+			{
+				java.lang.reflect.Constructor constructor = c.getConstructor(Node.class, Location.class);
+				
+				ModifierAnnotation a = (ModifierAnnotation)constructor.newInstance(null, null);
+				
+				String[] aliases = a.getAliases();
+				
+				for (String alias : aliases)
+				{
+					MODIFIERS.put(alias, constructor);
+				}
+			}
+			catch (NoSuchMethodException e)
+			{
+				e.printStackTrace();
+			}
+			catch (IllegalAccessException e)
+			{
+				e.printStackTrace();
+			}
+			catch (InstantiationException e)
+			{
+				e.printStackTrace();
+			}
+			catch (InvocationTargetException e)
+			{
+				e.printStackTrace();
+			}
+		});
+	}
 	/**
 	 * @see net.fathomsoft.nova.tree.Node#Node(Node, Location)
 	 */
 	public Annotation(Node temporaryParent, Location locationIn)
 	{
 		super(temporaryParent, locationIn);
+	}
+	
+	public String[] defaultParameterNames()
+	{
+		return new String[0];
+	}
+	
+	public String writeParameters()
+	{
+		String output = "";
+		
+		for (Map.Entry<String, Object> param : parameters.entrySet())
+		{
+			String value;
+			
+			if (param.getValue() instanceof Node)
+			{
+				value = ((Node)param.getValue()).generateNovaInput().toString();
+			}
+			else
+			{
+				value = param.getValue().toString();
+			}
+			
+			if (output.length() > 0)
+			{
+				output += ", ";
+			}
+			
+			output += param.getKey() + ": " + value;
+		}
+		
+		return output.trim();
+	}
+	
+	@Override
+	public String toString()
+	{
+		String name = getClass().getSimpleName();
+		name = name.substring(0, name.length() - "Annotation".length());
+		
+		String parameters = writeParameters();
+		
+		return "[" + name + (parameters.length() > 0 ? " " + parameters : "") + "]";
+	}
+	
+	public boolean parseParameter(String parameter, int index, boolean[] parsedExplicit)
+	{
+		int colon = SyntaxUtils.findCharInBaseScope(parameter, ':');
+		
+		String assigned;
+		
+		if (colon < 0 && parsedExplicit[0])
+		{
+			return SyntaxMessage.queryError("Must explicitly specify parameter name for '" + parameter + "'", this, true);
+		}
+		else if (colon < 0)
+		{
+			assigned = defaultParameterNames()[index];
+		}
+		else
+		{
+			assigned = parameter.substring(0, colon).trim();
+		}
+		
+		String assignment = parameter.substring(colon + 1).trim();
+		
+		if (!SyntaxUtils.isValidIdentifier(assigned))
+		{
+			return SyntaxMessage.queryError("Invalid annotation parameter name '" + assigned + "'", this, true);
+		}
+		
+		Value value = SyntaxTree.decodeValue(getParent(), assignment, getLocationIn(), true);
+		
+		if (value == null)
+		{
+			return SyntaxMessage.queryError("Invalid annotation parameter value '" + assignment + "'", this, true);
+		}
+		
+		parameters.put(assigned, value);
+		
+		return true;
+	}
+	
+	public boolean parseParameters(String parameters)
+	{
+		final boolean[] parsedExplicit = new boolean[] { false };
+		final int[] index = new int[] { 0 };
+		
+		return Arrays.stream(StringUtils.splitCommas(parameters)).allMatch(parameter -> {
+			return parseParameter(parameter, index[0]++, parsedExplicit);
+		});
 	}
 	
 	public Annotation getAnnotationOfType(Class type)
@@ -123,23 +274,28 @@ public class Annotation extends Node
 										
 										if (n == null)
 										{
-											n = KeepWhitespaceAnnotation.decodeStatement(parent, name, arguments, location, require);
+											n = ImmutableAnnotation.decodeStatement(parent, name, arguments, location, require);
 											
 											if (n == null)
 											{
-												n = ObsoleteAnnotation.decodeStatement(parent, name, arguments, location, require);
+												n = KeepWhitespaceAnnotation.decodeStatement(parent, name, arguments, location, require);
 												
 												if (n == null)
 												{
-													n = OverrideAnnotation.decodeStatement(parent, name, arguments, location, require);
+													n = ObsoleteAnnotation.decodeStatement(parent, name, arguments, location, require);
 													
 													if (n == null)
 													{
-														n = TargetAnnotation.decodeStatement(parent, name, arguments, location, require);
+														n = OverrideAnnotation.decodeStatement(parent, name, arguments, location, require);
 														
 														if (n == null)
 														{
-															n = NativeAnnotation.decodeStatement(parent, name, arguments, location, require);
+															n = TargetAnnotation.decodeStatement(parent, name, arguments, location, require);
+															
+															if (n == null)
+															{
+																n = NativeAnnotation.decodeStatement(parent, name, arguments, location, require);
+															}
 														}
 													}
 												}
@@ -180,7 +336,7 @@ public class Annotation extends Node
 		{
 			if (getParent() == null)
 			{
-				missingExpression(this, true);
+				missingExpression(true);
 			}
 		}
 		catch (SyntaxErrorException e)
@@ -229,7 +385,7 @@ public class Annotation extends Node
 		{
 			if (getParent() != null)
 			{
-				getLocationIn().setLineNumber(getLocationIn().getLineNumber() - getParent().getLineNumber());
+//				getLocationIn().setLineNumber(getLocationIn().getLineNumber() - getParent().getLineNumber());
 			}
 			//if (next instanceof Annotation)
 			{
@@ -245,30 +401,35 @@ public class Annotation extends Node
 		return SyntaxMessage.queryError(getClass().getSimpleName() + " is applied to an invalid type '" + node.getClass().getSimpleName() + "'", node, require);
 	}
 	
-	public static boolean missingExpression(Annotation node, boolean require)
+	public boolean invalidArgument(String name, boolean require)
 	{
-		return SyntaxMessage.queryError("Annotation is missing an expression to be applied to", node, require);
+		return SyntaxMessage.queryError(getClass().getSimpleName() + " given invalid argument '" + name + "'", this, require);
 	}
 	
-	public static boolean requiresArguments(Annotation node, boolean require)
+	public boolean missingExpression(boolean require)
 	{
-		return SyntaxMessage.queryError("Missing annotation arguments", node, require);
+		return SyntaxMessage.queryError(getClass().getSimpleName() + " is missing an expression to be applied to", this, require);
 	}
 	
-	public static boolean tooManyArguments(Annotation node, boolean require)
+	public boolean requiresArguments(boolean require)
 	{
-		return SyntaxMessage.queryError("Too many arguments given to annotation", node, require);
+		return SyntaxMessage.queryError("Missing annotation arguments", this, require);
+	}
+	
+	public boolean tooManyArguments(boolean require)
+	{
+		return SyntaxMessage.queryError("Too many arguments given to annotation", this, require);
 	}
 	
 	/**
 	 * @see net.fathomsoft.nova.tree.Node#clone(Node, Location, boolean)
 	 */
 	@Override
-	public Annotation clone(Node temporaryParent, Location locationIn, boolean cloneChildren)
+	public Annotation clone(Node temporaryParent, Location locationIn, boolean cloneChildren, boolean cloneAnnotations)
 	{
 		Annotation node = new Annotation(temporaryParent, locationIn);
 		
-		return cloneTo(node, cloneChildren);
+		return cloneTo(node, cloneChildren, cloneAnnotations);
 	}
 	
 	/**
@@ -276,7 +437,7 @@ public class Annotation extends Node
 	 */
 	public Annotation cloneTo(Annotation node)
 	{
-		return cloneTo(node, true);
+		return cloneTo(node, true, true);
 	}
 	
 	/**
@@ -286,9 +447,9 @@ public class Annotation extends Node
 	 * @param node The node to copy the data into.
 	 * @return The cloned node.
 	 */
-	public Annotation cloneTo(Annotation node, boolean cloneChildren)
+	public Annotation cloneTo(Annotation node, boolean cloneChildren, boolean cloneAnnotations)
 	{
-		super.cloneTo(node, cloneChildren);
+		super.cloneTo(node, cloneChildren, cloneAnnotations);
 		
 		return node;
 	}
