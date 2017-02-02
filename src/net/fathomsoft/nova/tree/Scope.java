d64cb231ee0cb4e77488f394937fdde65fd61f20
BSteffaniak <BradenSteffaniak@gmail.com>
2017-02-02 00:59:48 -0800

    Updated scope gen variables to be tagged as non-usermade

--- src/net/fathomsoft/nova/tree/Scope.java
+++ src/net/fathomsoft/nova/tree/Scope.java
@@ -294,6 +294,7 @@ public class Scope extends Node
 		LocalDeclaration decl = new LocalDeclaration(type.getParent(), type.getLocationIn());
 		
 		decl.setName("nova_local_" + getParentMethod().getScope().localVariableID++);
+		decl.setProperty("userMade", false);
 		decl.setForceOriginalName(true);
 		decl.setType(type.getReturnedNode().getNovaTypeValue(type.getReturnedNode()));
 		
