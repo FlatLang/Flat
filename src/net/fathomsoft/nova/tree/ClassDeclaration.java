9304430369faa81a72e3cd6ce6df4b1e7c79e291
BSteffaniak <BradenSteffaniak@gmail.com>
2017-02-04 03:36:45 -0800

    Fixed null pointer for containsMethod

--- src/net/fathomsoft/nova/tree/ClassDeclaration.java
+++ src/net/fathomsoft/nova/tree/ClassDeclaration.java
@@ -3923,7 +3923,7 @@ public class ClassDeclaration extends InstanceDeclaration
 		{
 			MethodDeclaration methodDeclaration = (MethodDeclaration)root.getChild(i);
 			
-			if (methodDeclaration.isStatic() == staticVal && methodDeclaration.getName().equals(methodName) && methodDeclaration.getType().equals(type))
+			if (methodDeclaration.isStatic() == staticVal && methodDeclaration.getName().equals(methodName) && (methodDeclaration.getType() == null && type == null || methodDeclaration.getType().equals(type)))
 			{
 				return true;
 			}
