85835aed2c10e179b4034abd85035f3be53b272c
BSteffaniak <BradenSteffaniak@gmail.com>
2017-02-22 06:03:21 -0800

    Updated to parse closure in place of type

--- src/net/fathomsoft/nova/tree/FunctionType.java
+++ src/net/fathomsoft/nova/tree/FunctionType.java
@@ -4,9 +4,39 @@ public class FunctionType extends Type
 {
 	public Type type;
 	
+	ClosureDeclaration closure;
+	
+	public static FunctionType parse(Node parent, String value)
+	{
+		if (value.indexOf('(') > 0)
+		{
+			ClosureDeclaration c = ClosureDeclaration.decodeStatement(parent, value, parent.getLocationIn(), false);
+			
+			if (c != null)
+			{
+				FunctionType type = new FunctionType();
+				
+				type.value = c.getName();
+				type.type = c.getTypeObject();
+				type.closure = c;
+				
+				return type;
+			}
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public String toNova()
+	{
+		return closure.generateNovaInput().toString();
+	}
+	
 	public FunctionType cloneTo(FunctionType clone)
 	{
 		clone.type = type != null ? type.clone() : null;
+		clone.closure = closure;
 		
 		return clone;
 	}
