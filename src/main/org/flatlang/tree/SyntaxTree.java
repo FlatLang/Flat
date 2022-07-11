c91aef318902523cbb4fc785d869ec30fbb919a2
Braden Steffaniak <BradenSteffaniak@gmail.com>
2022-07-11 17:14:22 -0400

    Delete ChainedMethodCall

--- src/main/org/flatlang/tree/SyntaxTree.java
+++ src/main/org/flatlang/tree/SyntaxTree.java
@@ -64,8 +64,8 @@ public class SyntaxTree
 		Annotation.class, Break.class, Default.class, Case.class, Match.class,
 		Fallthrough.class, Continue.class, LambdaExpression.class, CodeBlock.class, ExceptionHandler.class,
 		Throw.class, Assignment.class, Instantiation.class, ElseStatement.class, IfStatement.class,
-		Until.class, Loop.class, Array.class, Cast.class, ChainedMethodCall.class, MethodCall.class,
-		LocalDeclaration.class, ExternalCodeBlock.class, Array.class, ClosureVariable.class,
+		Until.class, Loop.class, Array.class, Cast.class, MethodCall.class, LocalDeclaration.class,
+		ExternalCodeBlock.class, Array.class, ClosureVariable.class,
 	};
 	
 	public static final Class<?> FIELD_SCOPE_CHILD_DECODE[] = new Class<?>[]
@@ -605,7 +605,6 @@ public class SyntaxTree
 				else if (node == null && type == Array.class) node = Array.decodeStatement(parent, statement, location, require);
 				else if (node == null && type == Case.class) node = Case.decodeStatement(parent, statement, location, require);
 				else if (node == null && type == Cast.class) node = Cast.decodeStatement(parent, statement, location, require);
-				else if (node == null && type == ChainedMethodCall.class) node = ChainedMethodCall.decodeStatementValue(parent, statement, location, require);
 				else if (node == null && type == BinaryOperation.class) node = BinaryOperation.decodeStatement(parent, statement, location, require);
 				else if (node == null && type == Break.class) node = Break.decodeStatement(parent, statement, location, require);
 				else if (node == null && type == ClosureVariable.class) node = ClosureVariable.decodeStatement(parent, statement, location, require);
@@ -717,7 +716,6 @@ public class SyntaxTree
 //		else if (type.isAssignableFrom(Bool.class) && (node = Bool.decodeStatement(parent, statement, location, require)) != null);
 		else if (type.isAssignableFrom(Break.class) && (node = Break.decodeStatement(parent, statement, location, require)) != null);
 		else if (type.isAssignableFrom(Case.class) && (node = Case.decodeStatement(parent, statement, location, require)) != null);
-		else if (type.isAssignableFrom(ChainedMethodCall.class) && (node = ChainedMethodCall.decodeStatementValue(parent, statement, location, require)) != null);
 		else if (type.isAssignableFrom(ClosureVariable.class) && (node = ClosureVariable.decodeStatement(parent, statement, location, require)) != null);
 		else if (type.isAssignableFrom(Continue.class) && (node = Continue.decodeStatement(parent, statement, location, require)) != null);
 		else if (type.isAssignableFrom(BinaryOperation.class) && (node = BinaryOperation.decodeStatement(parent, statement, location, require)) != null);
@@ -786,7 +784,6 @@ public class SyntaxTree
 		if      (type2.isAssignableFrom(AbstractMethodDeclaration.class) == declaration && type.isAssignableFrom(AbstractMethodDeclaration.class) && (node = AbstractMethodDeclaration.decodeStatement(parent, statement, location, require)) != null);
 		else if (type2.isAssignableFrom(ArrayAccess.class) == declaration && type.isAssignableFrom(ArrayAccess.class) && (node = ArrayAccess.decodeStatement(parent, statement, location, require)) != null);
 		else if (type2.isAssignableFrom(Array.class) == declaration && type.isAssignableFrom(Array.class) && (node = Array.decodeStatement(parent, statement, location, require)) != null);
-		else if (type2.isAssignableFrom(ChainedMethodCall.class) == declaration && type.isAssignableFrom(ChainedMethodCall.class) && (node = ChainedMethodCall.decodeStatementValue(parent, statement, location, require)) != null);
 		else if (type2.isAssignableFrom(ClassDeclaration.class) == declaration && type.isAssignableFrom(ClassDeclaration.class) && (node = ClassDeclaration.decodeStatement(parent, statement, location, require)) != null);
 		else if (type2.isAssignableFrom(Closure.class) == declaration && type.isAssignableFrom(Closure.class) && (node = Closure.decodeStatement(parent, statement, location, require)) != null);
 		else if (type2.isAssignableFrom(ClosureVariable.class) == declaration && type.isAssignableFrom(ClosureVariable.class) && (node = ClosureVariable.decodeStatement(parent, statement, location, require)) != null);
