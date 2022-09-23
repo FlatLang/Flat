e833b92d821d7b7cf107cf8a3d4076dda16927d1
Braden Steffaniak <braden@novex.tech>
2022-09-23 10:56:03 -0400

    Use shorthand Builder type

--- src/main/flat/tree/DataClassDeclaration.java
+++ src/main/flat/tree/DataClassDeclaration.java
@@ -299,13 +299,13 @@ public class DataClassDeclaration extends ClassDeclaration
 
 		addChild(builderClass);
 
-		BodyMethodDeclaration toBuilderFunc = BodyMethodDeclaration.decodeStatement(builderClass, "public toBuilder() -> " + builderClass.getClassLocation(false, false), Location.INVALID, true);
+		BodyMethodDeclaration toBuilderFunc = BodyMethodDeclaration.decodeStatement(builderClass, "public toBuilder() -> Builder", Location.INVALID, true);
 
 		String toBuilderArgs = fields.stream()
 			.map(f -> f.getName() + ": " + f.getName())
 			.collect(Collectors.joining(", "));
 
-		toBuilderFunc.shorthandAction = builderClass.getClassLocation(false, false) + "(" + toBuilderArgs + ")";
+		toBuilderFunc.shorthandAction = "Builder(" + toBuilderArgs + ")";
 
 		addChild(toBuilderFunc);
 	}
