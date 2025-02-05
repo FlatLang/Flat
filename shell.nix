2d1abeb2344780720276dc9a8c7b2c0d8f0ccb07
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-02-05 07:30:00 -0500

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,13 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  pkgs = import nixpkgs {
+    config = { };
+    overlays = [ ];
+  };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    jdk21
+  ];
+}
